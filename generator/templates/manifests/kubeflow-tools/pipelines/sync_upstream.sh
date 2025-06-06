14e923f2ef46a6280b115272a887feda2ea07534
Edi Bice <edi.bice@blueyonder.com>
2025-06-06 08:53:09 -0400

    attempt at updating to upstream kubeflow 1.10

--- generator/templates/manifests/kubeflow-tools/pipelines/sync_upstream.sh
+++ generator/templates/manifests/kubeflow-tools/pipelines/sync_upstream.sh
@@ -8,7 +8,9 @@ cd "$THIS_SCRIPT_PATH"
 # upstream configs
 UPSTREAM_REPO="github.com/kubeflow/pipelines"
 UPSTREAM_PATH="manifests/kustomize/env/cert-manager/platform-agnostic-multi-user"
-UPSTREAM_REF="f328f0b588c35cdf1e5b31638fbe3596b2f38413" # v2.1.0
+#UPSTREAM_REF="f328f0b588c35cdf1e5b31638fbe3596b2f38413" # v2.1.0
+#UPSTREAM_REF="4003e562713bd04fa94387d8b53dfbe3cf31cb12" #v2.3.0
+UPSTREAM_REF="242d025285c4b092aa561947d45feb67938a102d" #v2.4.1
 
 # output configs
 OUTPUT_PATH="./upstream"
--- scripts/sync_argocd_apps.sh
+++ scripts/sync_argocd_apps.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
+#!/opt/homebrew/bin/bash
+#####!/usr/bin/env bash
 
 set -euo pipefail
 
